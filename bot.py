f807ce168cbe5de452c6467b2ae0c0263b0bca41
Anonimbus <38459708+TarushS@users.noreply.github.com>
2021-06-18 19:35:21 +0530

    removed seleinum blood

--- bot.py
+++ bot.py
@@ -10,15 +10,6 @@ import json
 load_dotenv()
 
 
-'''
-global ch
-ch =  {23: {'name': 'TEST_1', 'solved': False}}
-'''
-
-global connectionData
-connectionData = {}
-
-
 client = commands.Bot(command_prefix='.')
 
 client.remove_command('help')
@@ -35,78 +26,6 @@ for filename in os.listdir('./cogs'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 
-
-
-'''
-@tasks.loop(seconds=180)
-async def firstBlood():
-
-    allSolved = True
-    keys = dict.keys(ch)
-    for i in keys:
-        if not(ch[i]['solved']):
-            allSolved = False
-
-    if(allSolved):
-        return
-    
-    channel = client.get_channel(int(os.getenv('FIRST_BLOOD_CHANNEL')))
-
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--single-processu')
-
-
-    usernameStr = os.getenv('USER')
-    passwordStr = os.getenv('PASSWORD')
-
-    browser = webdriver.Chrome(ChromeDriverManager().install(), options=options )
-
-    print('loading')
-    browser.get(('htssp://ctf.tarush.co/login'))
-    print('loaded')
-
-    username = browser.find_element_by_id('name')
-    username.send_keys(usernameStr)
-    password = browser.find_element_by_id('password')
-    password.send_keys(passwordStr)
-
-    submitBtn = browser.find_element_by_id('_submit')
-    submitBtn.click()
-
-    for i in keys:
-        
-        if(ch[i]['solved']):
-            continue
-
-        browser.get((f'https://ctf.tarush.co/api/v1/challenges/{i}/solves'))
-        html = browser.page_source
-        time.sleep(2)
-        html = html[html.index('{'):html.rindex('}')+1]
-        print(html)
-        y = json.loads(html)
-            
-        try: y['data']
-        except:
-            print('key error')
-            continue
-
-        if(y['data']==[]):
-            continue
-
-        if(y['data']==[]):
-            print(f'no data for {ch[i]}')
-            continue
-
-        ch[i]['solved'] = True
-        # print(f'`First blood for challenge: {ch[i]["name"]} goes to {y["data"][0]["name"]}`')
-        print("sending")
-        await channel.send(f'```css\n🩸 First blood for .{ch[i]["name"]} goes to [{y["data"][0]["name"]}]```')
-    browser.close()
-    print('Completed!')
-'''
 @client.command(aliases=['Flag'])
 async def flag(ctx):
     if ctx.channel.type is discord.ChannelType.private:
